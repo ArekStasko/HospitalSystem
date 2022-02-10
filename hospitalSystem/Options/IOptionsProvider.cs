1ce00bf9ef32ea7525783edd2b8a0fd48ee3826d
ArekStasko <arek.stasko@gmail.com>
2022-02-10 22:36:14 +0100

    Prepare project to refactoring

---
+++ hospitalSystem/Options/IOptionsProvider.cs
@@ -0,0 +1,13 @@
+﻿
+namespace HospitalSystem.Views
+{
+    public interface IOptionsProvider
+    {
+        public void PrintStartingOptions();
+        public void PrintAdminOptions();
+        public void PrintDoctorOptions();
+        public void PrintPatientOptions();
+        public void PrintHospitalOptions();
+
+    }
+}
---
+++ hospitalSystem/Options/OptionsProvider.cs
@@ -0,0 +1,76 @@
+﻿
+namespace HospitalSystem.Views
+{
+    public class OptionsProvider : IOptionsProvider
+    {
+        public void PrintStartingOptions()
+        {
+            string[] startingOptions = new string[]{ "Patient", "Doctor", "Admin" };
+
+            Console.WriteLine("You want to login as :");
+            PrintOptions(startingOptions);
+        }
+
+        public void PrintAdminOptions()
+        {
+            string[] adminOptions = new string[] 
+            { 
+                "Show Hospitals with Doctors", 
+                "Show Visits",
+                "Add Doctor", 
+                "Remove Doctor", 
+                "Add Hospital", 
+                "Remove Hospital",
+                "Add Visit",
+                "Remove visit",
+                "Close System"
+            };
+            PrintOptions(adminOptions);
+        }
+
+        public void PrintDoctorOptions()
+        {
+            string[] doctorOptions = new string[]
+            {
+                "Show my hospital",
+                "Show visits assigned to me",
+                "Close System"
+            };
+            PrintOptions(doctorOptions);
+        }
+
+        public void PrintPatientOptions()
+        {
+            string[] patientOptions = new string[]
+            {
+                "Show hospital options",
+                "Show my visits",
+                "Sign up for visit",
+                "Close System"
+            };
+            PrintOptions(patientOptions);
+        }
+
+        public void PrintHospitalOptions()
+        {
+            string[] hospitalOptions = new string[]
+            {
+                "Change my hospital",
+                "Show main info about my hospital",
+                "Show doctors assinged to my hospital",
+                "Show available visits"
+            };
+            PrintOptions(hospitalOptions);
+        }
+
+        private void PrintOptions(string[] options)
+        {
+            Console.WriteLine("What do you want to do ? :");
+            for (int i = 0; i < options.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {options[i]}");
+            }
+            Console.WriteLine("Choose number of option :");
+        }
+    }
+}
